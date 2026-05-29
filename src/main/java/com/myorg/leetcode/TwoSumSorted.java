dc5b7c4d879c5460775d5bf5858ffc28ed81dabd
crdSDE18 <crdoriety110417@gmail.com>
2026-05-29 02:27:20 -0500

    added docs

--- src/main/java/com/myorg/leetcode/TwoSumSorted.java
+++ src/main/java/com/myorg/leetcode/TwoSumSorted.java
@@ -2,14 +2,22 @@ package com.myorg.leetcode;
 
 public class TwoSumSorted {
 
+    /**
+     *  The solution's time complexity is average O(n), because the array is sorted, i used the 2 method approach. I start from the front and pointer at the
+     *  end, and i move pointer based on value of current sum and target.
+     * @param numbers
+     * @param target
+     * @return
+     */
+
     public int[] twoSumSorted(int[] numbers, int target){
         int lp=0, rp= numbers.length - 1;
 
         while(lp <rp){
-            int sum = numbers[lp] + numbers[rp];
-            if(sum == target){
+            int currentSum = numbers[lp] + numbers[rp];
+            if(currentSum == target){
                 return new int[]{lp+1,rp+1};
-            } else if (sum < target) {
+            } else if (currentSum < target) {
                 lp++;
             }
             else{
