1c376e26ebf18277e09591f0857b1aefd6ffbb86
crdSDE18 <crdoriety110417@gmail.com>
2026-06-06 05:32:37 -0500

    commit out first solution

--- src/main/java/com/myorg/leetcode/slidingwindow/MaxSubArray.java
+++ src/main/java/com/myorg/leetcode/slidingwindow/MaxSubArray.java
@@ -7,44 +7,46 @@ public class MaxSubArray {
 
     /**
      * the maxSubArray() is the optimized version, the maxSubArrayFirstPass was the first solution after creating tests
+     *
      * @param nums
      * @return
      */
-    public int maxSubArray(int[] nums){
-        if(nums == null){
+    public int maxSubArray(int[] nums) {
+        if (nums == null) {
             throw new RuntimeException("Input array is empty");
         }
 
-        int maxSum= nums[0];
+        int maxSum = nums[0];
         int currentSum = nums[0];
 
-        for(int i=1; i< nums.length; i++){
-            currentSum = Math.max(nums[i], currentSum +nums[i]);
-            maxSum = Math.max(maxSum,currentSum);
-        }
-        return  maxSum;
-    }
-
-
-
-    public int maxSubArrayFirstPass(int[] nums) {
-        if (nums == null) {
-            return 0;
-        }
-        if (nums.length == 1) {
-            return nums[0];
-        }
-
-        int maxSum = Integer.MIN_VALUE;
-        int currentSum =0;
-        for (int i = 0; i < nums.length; i++) {
-           currentSum += nums[i];
-
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
         }
-            return maxSum;
+        return maxSum;
     }
 }
+
+
+
+//    public int maxSubArrayFirstPass(int[] nums) {
+//        if (nums == null) {
+//            return 0;
+//        }
+//        if (nums.length == 1) {
+//            return nums[0];
+//        }
+//
+//        int maxSum = Integer.MIN_VALUE;
+//        int currentSum =0;
+//        for (int i = 0; i < nums.length; i++) {
+//           currentSum += nums[i];
+//
+//            maxSum = Math.max(maxSum, currentSum);
+//            if (currentSum < 0) {
+//                currentSum = 0;
+//            }
+//        }
+//            return maxSum;
+//    }
+//}
