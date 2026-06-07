830c4e0c01d414671ef9dd22ff7d7d32e62312fe
crdSDE18 <crdoriety110417@gmail.com>
2026-06-07 04:41:44 -0500

    best time to buy and sell with test

--- src/main/java/com/myorg/leetcode/slidingwindow/BestTimeToBuySell.java
+++ src/main/java/com/myorg/leetcode/slidingwindow/BestTimeToBuySell.java
@@ -1,2 +1,35 @@
-package com.myorg.leetcode.slidingwindow;public class BestTimeToBuySell {
+package com.myorg.leetcode.slidingwindow;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class BestTimeToBuySell {
+
+    public int maxProfit(int[] prices){
+        int maxProfit=0;
+        int minPrice=prices[0];
+        for(int i=1; i<prices.length; i++){
+            minPrice = Math.min(minPrice,prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i]- minPrice);
+
+        }
+        return maxProfit;
+    }
+
+//    public int maxProfitBrutForce(int[] prices){
+//        int maxProfit = 0;
+//        for(int i=0; i< prices.length; i++){
+//            for(int j=i+1; j<prices.length; j++){
+//                int currentProfit = prices[j]-prices[i];
+//                if(currentProfit >=0){
+//                    log.info("current profit is greater than zero: {}", currentProfit);
+//                    maxProfit = Math.max(maxProfit,currentProfit);
+//                }
+//            }
+//        }
+//        return maxProfit;
+//    }
+
 }
--- src/test/java/com/myorg/leetcode/slidingwindow/BestTimeToBuySellTest.java
+++ src/test/java/com/myorg/leetcode/slidingwindow/BestTimeToBuySellTest.java
@@ -1,2 +1,47 @@
-package com.myorg.leetcode.slidingwindow;public class BestTimeToBuySellTest {
+package com.myorg.leetcode.slidingwindow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BestTimeToBuySellTest {
+
+    private final BestTimeToBuySell solution = new BestTimeToBuySell();
+
+    @Test
+    void shouldReturnFiveWhenBuyingAtLowestPriceAndSellingAtHighestLaterPrice() {
+        assertEquals(5,
+                solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    @Test
+    void shouldReturnZeroWhenPricesContinuouslyDecline() {
+        assertEquals(0,
+                solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+
+    @Test
+    void shouldReturnZeroForSingleTradingDay() {
+        assertEquals(0,
+                solution.maxProfit(new int[]{5}));
+    }
+
+    @Test
+    void shouldReturnMaximumProfitForIncreasingPrices() {
+        assertEquals(4,
+                solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    void shouldReturnZeroWhenPricesRemainConstant() {
+        assertEquals(0,
+                solution.maxProfit(new int[]{3, 3, 3, 3}));
+    }
+
+    @Test
+    void shouldIdentifyBestBuyAndSellOpportunityInMiddleOfArray() {
+        assertEquals(8,
+                solution.maxProfit(new int[]{8, 6, 2, 5, 10}));
+    }
 }
+
